2ad59f4ffc3123e578181b190d76205a1daffac2
Sarthak2 <aniki123.ai@gmail.com>
2025-04-05 17:01:22 +0530

    added schedule message

--- SHA256.cpp
+++ SHA256.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstdint>
 
 class SHA256{
 	public:
+		int ROTR(int x, int y){
+			return (x >> y) | (x << (32 - y));
+		}
+
+		int SHR(int x, int y){
+			return x >> y;
+		}
+
+		int sigma0(int x){
+			return ROTR(x, 7) ^ ROTR(x, 18) ^ SHR(x, 3);
+		}
+
+		int sigma1(int x){
+			return ROTR(x, 17) ^ ROTR(x, 19) ^ SHR(x, 10);
+		}
+
 		std::vector<int> getBinary(std::string& message){
 			std::vector<int> bits;
 
@@ -19,7 +36,7 @@ class SHA256{
 			bits.push_back(1); 								// stored like: ['1','0','1','1','0','0','0'] + 1 // Add 1 for padding
 		 	int bits_size = bits.size();
 			// 512 - 64 (holds original) = 448
-			int padding_size = 448 - (bits_size % 512); 		// if its for ex 335 we append 335 zeros
+			int padding_size = 448 - (bits_size % 512); 	// if its for ex 335 we append 335 zeros
 			for(int i=0; i<padding_size; i++){
 				bits.push_back(0);
 			}
@@ -29,7 +46,7 @@ class SHA256{
         		}
 
         		return bits;
-	}
+		}
 
 		std::vector<std::vector<int>> getChunks(std::vector<int>& bits){
 			std::vector<std::vector<int>> chunks;
@@ -42,5 +59,18 @@ class SHA256{
 				chunks.push_back(chunk);
 			}
 			return chunks;
+		}
+
+		std::vector<int> scheduleMessage(std::vector<int>& bits){
+			std::vector<std::vector<int>> W;
+
+			for(int i=0; i<16; i++){
+				std::vector<int> word;
+				for(int j=0; j<32; j++){
+					word.push_back(bits[i * 32 * j]);
+				}
+				W.push_back(word);
+			}
+			return W;
 		}
 };
